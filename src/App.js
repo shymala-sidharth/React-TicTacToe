b37a89ef5a7a6c4f3947efd76c394cec35e62508
Shymala Sidharth <shymala.sidharth22@gmail.com>
2023-04-08 13:24:10 +1200

    working through react docs

--- src/App.js
+++ src/App.js
@@ -14,12 +14,34 @@ export default function Board() {
   const [squares, setSquares] = useState(Array(9).fill(null))
 
   function handleClick(e) {
+    if (squares[e] || calculateWinner(squares)) {
+      return
+    }
     const nextSquare = [...squares]
     xIsNext ? (nextSquare[e] = 'X') : (nextSquare[e] = 'O')
 
     setSquares(nextSquare)
     setXisNext(!xIsNext)
     console.log('clicked')
+  }
+
+  function calculateWinner(squares) {
+    const lines = [
+      [0, 1, 2],
+      [3, 4, 5],
+      [6, 7, 8],
+      [0, 3, 7],
+      [1, 4, 7],
+      [2, 4, 8],
+      [0, 4, 8],
+      [2, 4, 6],
+    ]
+
+    const winningLine = lines.find(
+      ([a, b, c]) =>
+        squares[a] && squares[a] === squares[b] && squares[a] === squares[c]
+    )
+    return winningLine ? squares[winningLine[0]] : null
   }
 
   return (
